7081f691d376891236ae7ae5e95e6146be12644c
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-05-10 09:22:34 -0400

    test 2

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,16 @@
-import 'phaser'
-import MainScene from './scenes/mainScene'
-import PreloadScene from './scenes/preloadScene'
-
-const DEFAULT_WIDTH = 1280
-const DEFAULT_HEIGHT = 720
+import MainScene from '../scenes/mainScene';
 
 const config = {
   type: Phaser.AUTO,
-  backgroundColor: '#ffffff',
-  scale: {
-    parent: 'phaser-game',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: DEFAULT_WIDTH,
-    height: DEFAULT_HEIGHT
-  },
-  scene: [PreloadScene, MainScene],
+  width: 800,
+  height: 600,
+  scene: MainScene,
   physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false,
-      gravity: { y: 400 }
-    }
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 200 }
+      }
   }
-}
+};
 
-window.addEventListener('load', () => {
-  const game = new Phaser.Game(config)
-})
+const game = new Phaser.Game(config);
