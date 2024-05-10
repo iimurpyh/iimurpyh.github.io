168fcb97ecd3c434014853efe1c94319ca00b8fe
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-05-10 10:39:54 -0400

    move to subdomain

--- gandoff/scripts/objects/player.js
+++ gandoff/scripts/objects/player.js
@@ -1,11 +1,10 @@
-export default class PhaserLogo extends Phaser.Physics.Arcade.Sprite {
+export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
-    super(scene, x, y, 'phaser-logo')
+    super(scene, x, y, 'player')
     scene.add.existing(this)
     scene.physics.add.existing(this)
 
     this.setCollideWorldBounds(true)
-      .setBounce(0.6)
       .setInteractive()
       .on('pointerdown', () => {
         this.setVelocityY(-400)
