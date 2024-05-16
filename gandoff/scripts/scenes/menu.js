6a95f72b6392e86d4d47de8a9ab696ce90a37fda
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-05-16 09:41:15 -0400

    Add files via upload

--- gandoff/scripts/scenes/menu.js
+++ gandoff/scripts/scenes/menu.js
@@ -1,9 +1,28 @@
 export default class MenuScene extends Phaser.Scene {
+    static menuStates = ['menu-main', 'menu-host', 'menu-join']
+
     constructor() {
       super({ key: 'MenuScene' });
     }
 
     create() {
-      this.add.dom(this.cameras.main.width / 2, this.cameras.main.height / 2).createFromCache('menu');
+      this._menu = this.add.dom(this.cameras.main.width / 2, this.cameras.main.height / 2).createFromCache('menu');
+
+      this._menu.addListener('click');
+
+      this._menu.on('click', function(event) {
+        if (event.target.name == 'menu-switch') {
+          let switchTo = event.target.dataset.switchTo;
+
+          for (let state of MenuScene.menuStates) {
+            this.getChildByID(state).style.display = 'none';
+          }
+          this.getChildByID(switchTo).style.display = 'flex';
+
+          if (switchTo == 'menu-host') {
+            
+          }
+        }
+      })
     }
   }
