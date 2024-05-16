53c727cf3e37ce7ba4c9a404e7e048790007b11e
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-05-16 12:42:00 -0400

    room code gen test

--- gandoff/scripts/scenes/menu.js
+++ gandoff/scripts/scenes/menu.js
@@ -7,8 +7,10 @@ export default class MenuScene extends Phaser.Scene {
 
     create() {
       this._menu = this.add.dom(this.cameras.main.width / 2, this.cameras.main.height / 2).createFromCache('menu');
-
       this._menu.addListener('click');
+
+      // Click callback is going to override 'this' so put it in a variable
+      const this_scene = this;
 
       this._menu.on('click', function(event) {
         if (event.target.name == 'menu-switch') {
@@ -20,7 +22,10 @@ export default class MenuScene extends Phaser.Scene {
           this.getChildByID(switchTo).style.display = 'flex';
 
           if (switchTo == 'menu-host') {
-            
+            // Generate a 5-digit hexidecimal string to make an alphanumeric code
+            let num = (Math.random() * 0xFFFFF-1) + 1
+            this_scene._roomCode = num.toString(16);
+            this.getChildByID('room-code-highlight').innerHTML = this_scene._roomCode;
           }
         }
       })
