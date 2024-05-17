8e96d70a35b234e8d34ce008206c9f782c461d49
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-05-17 08:56:33 -0400

    working on networking

--- gandoff/scripts/scenes/menu.js
+++ gandoff/scripts/scenes/menu.js
@@ -1,35 +1,57 @@
 export default class MenuScene extends Phaser.Scene {
-    static menuStates = ['menu-main', 'menu-host', 'menu-join']
+  static menuStates = ['menu-main', 'menu-host', 'menu-join']
 
-    constructor() {
-      super({ key: 'MenuScene' });
-    }
+  constructor() {
+    super({ key: 'MenuScene' });
+  }
 
-    create() {
-      this._menu = this.add.dom(this.cameras.main.width / 2, this.cameras.main.height / 2).createFromCache('menu');
-      this._menu.addListener('click');
-
-      // Click callback is going to override 'this' so put it in a variable
-      const this_scene = this;
-
-      this._menu.on('click', function(event) {
-        if (event.target.name == 'menu-switch') {
-          let switchTo = event.target.dataset.switchTo;
-
-          for (let state of MenuScene.menuStates) {
-            this.getChildByID(state).style.display = 'none';
-          }
-          this.getChildByID(switchTo).style.display = 'flex';
-
-          if (switchTo == 'menu-host') {
-            // Generate string of 5 random alphabetical letters as PeerJS identifier
-            let roomCode = '';
-            for (let i = 0; i < 5; i++) {
-              roomCode += String.fromCharCode((Math.random()*26) + 65);
-            }
-            this.getChildByID('room-code-highlight').innerHTML = this_scene._roomCode;
-          }
-        }
-      })
+  _displayJoinError(errorMessage) {
+    let errorLabel = this._menu.getChildByID('join-error');
+    if (errorMessage != null) {
+      errorLabel.innerHTML = errorMessage;
+      errorLabel.style.display = 'block';
+    } else {
+      errorLabel.style.display = 'none';
     }
   }
+
+  create() {
+    // Generate string of 5 random alphabetical letters as PeerJS identifier
+    let roomCode = '';
+    for (let i = 0; i < 5; i++) {
+      roomCode += String.fromCharCode((Math.random()*26) + 65);
+    }
+    this._roomCode = roomCode;
+    // Create PeerJS connector
+    this._peer = new Peer(this._roomCode);
+    this._peer.debug = 3;
+
+    this._menu = this.add.dom(this.cameras.main.width / 2, this.cameras.main.height / 2).createFromCache('menu');
+    this._menu.addListener('click');
+
+    // Click callback is going to override 'this' so put it in a variable
+    const this_scene = this;
+
+    this._menu.on('click', function(event) {
+      if (event.target.name == 'menu-switch') {
+        let switchTo = event.target.dataset.switchTo;
+
+        for (let state of MenuScene.menuStates) {
+          this.getChildByID(state).style.display = 'none';
+        }
+        this.getChildByID(switchTo).style.display = 'flex';
+      } else if (event.target.name == 'connect') {
+        let code = this_scene._menu.getChildByID('room-code-entry').value;
+        const connection = this_scene._peer.connect(code.toUpperCase());
+        if (connection.open) {
+          this_scene._displayJoinError(null);
+        } else {
+          this_scene._displayJoinError('Peer not found.');
+        }
+      }
+    });
+
+    this._menu.getChildByID('room-code-highlight').innerHTML = this_scene._roomCode;
+    
+  }
+}
